557bdbec58e61e2013dbdad1ec1fbf7fd7a428a6
evgez <jzabrodin@gmail.com>
2014-03-03 20:05:38 +0200

    Hello

--- NewsReader/app/src/main/java/com/eugez/app/MainActivity.java
+++ NewsReader/app/src/main/java/com/eugez/app/MainActivity.java
@@ -1,19 +1,13 @@
 package com.eugez.app;
 
+import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
-import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
@@ -32,43 +26,43 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button   showFiles = (Button)findViewById(R.id.showFiles);
-        Button   loadFile  = (Button)findViewById(R.id.loadFiles);
-        EditText editText1 = (EditText)findViewById(R.id.editText);
+        Button showFiles = (Button) findViewById(R.id.showFiles);
+        Button loadFile = (Button) findViewById(R.id.loadFiles);
 
-        View.OnClickListener oclList    =  new View.OnClickListener() {
+        View.OnClickListener oclList = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText editText1 = (EditText)findViewById(R.id.editText);
-                EditText path1  = (EditText) findViewById(R.id.pat);
-                String   pathTo = path1.getText().toString();
-                File     f      = new File(pathTo);
-                File     file[] = f.listFiles();
-                String   conten = null;
-                for (int i = 0; i < file.length;i++)
-                {
-                    conten  = file[i].getName();
-                    editText1.setText(conten + "/n");
+                EditText editText1 = (EditText) findViewById(R.id.editText);
+                EditText path1 = (EditText) findViewById(R.id.pat);
+                try {
+                    String pathTo = path1.getText().toString();
+                    File f = new File(pathTo);
+                    File file[] = f.listFiles();
+                    String conten = null;
+                    for (int i = 0; i < file.length; i++) {
+                        conten = file[i].getName();
+                        editText1.setText(conten + "/n");
+                    }
+                } catch (NullPointerException e) {
+                    e.printStackTrace();
                 }
             }
         };
 
 
-
-        View.OnClickListener oclLoad    =   new View.OnClickListener() {
+        View.OnClickListener oclLoad = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText path1 = (EditText)findViewById(R.id.pat);
+                EditText path1 = (EditText) findViewById(R.id.pat);
                 XmlPullParserFactory pullParserFactory;
-                try{
-                    pullParserFactory    = XmlPullParserFactory.newInstance();
+                try {
+                    pullParserFactory = XmlPullParserFactory.newInstance();
                     XmlPullParser parser = pullParserFactory.newPullParser();
-                    String sdcard = Environment.DIRECTORY_DCIM;
-                    InputStream in_s     = getApplicationContext().getAssets().open(path1.getText().toString());
-                    parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES,false);
-                    parser.setInput  (in_s,null);
+                    InputStream in_s = getApplicationContext().getAssets().open(path1.getText().toString());
+                    parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+                    parser.setInput(in_s, null);
                     parseXML(parser);
-                }   catch (XmlPullParserException e){
+                } catch (XmlPullParserException e) {
                     e.printStackTrace();
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -85,7 +79,7 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
@@ -103,37 +97,36 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void parseXML(XmlPullParser parser) throws XmlPullParserException,IOException
-    {
-        ArrayList<Students>    students             = null;
-        ArrayList<Napravlenie> napravleniya         = null;
-        int                    eventType            = parser.getEventType();
-        Students               currentStudent       = null;
-        Napravlenie            currentNapravlenie   = null;
+    private void parseXML(XmlPullParser parser) throws XmlPullParserException, IOException {
+        ArrayList<Students> students = null;
+        ArrayList<Napravlenie> napravleniya = null;
+        int eventType = parser.getEventType();
+        Students currentStudent = null;
+        Napravlenie currentNapravlenie = null;
 
-        while (eventType != XmlPullParser.END_DOCUMENT){
+        while (eventType != XmlPullParser.END_DOCUMENT) {
             String name = null;
-            switch (eventType){
+            switch (eventType) {
                 case XmlPullParser.START_DOCUMENT:
                     students = new ArrayList<Students>();
                     break;
                 case XmlPullParser.START_TAG:
                     name = parser.getName();
-                    if (name == "student"){
-                       currentStudent   =   new Students();
-                    }  else if (currentStudent != null){
-                        if (name == "fio"){
-                            currentStudent.fio  =   parser.nextText();
-                        } else if (name == "id"){
-                            currentStudent.id   =   parser.nextText();
-                        } else if (name == "ratingPoints"){
+                    if (name.equals("student")) {
+                        currentStudent = new Students();
+                    } else if (currentStudent != null) {
+                        if (name == "fio") {
+                            currentStudent.fio = parser.nextText();
+                        } else if (name == "id") {
+                            currentStudent.id = parser.nextText();
+                        } else if (name == "ratingPoints") {
                             currentStudent.ratingPoints = parser.nextText();
                         }
                     }
-                break;
+                    break;
                 case XmlPullParser.END_TAG:
                     name = parser.getName();
-                    if (name.equalsIgnoreCase("student") && currentStudent != null){
+                    if (name.equalsIgnoreCase("student") && currentStudent != null) {
                         students.add(currentStudent);
                     }
             }
@@ -145,7 +138,7 @@ public class MainActivity extends ActionBarActivity {
     private void printStudents(ArrayList<Students> students) {
         String content = "";
         Iterator<Students> it = students.iterator();
-        while(it.hasNext()){
+        while (it.hasNext()) {
             Students currStud = it.next();
             content = content + currStud.fio + "/n";
             content = content + currStud.id + "/n";
@@ -153,7 +146,7 @@ public class MainActivity extends ActionBarActivity {
 
         }
 
-        EditText display = (EditText)findViewById(R.id.editText);
+        EditText display = (EditText) findViewById(R.id.editText);
         display.setText(content);
     }
 
